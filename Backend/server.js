e5d45d6bca660599b5335ec471a8fffca99861d9
Nossir-blue <rogerreis20@protonmail.com>
2025-03-23 12:28:19 +0100

    remaking the server because it was not getting the request properly

--- Backend/server.js
+++ Backend/server.js
@@ -7,53 +7,12 @@ const { Pool } = require('pg');
 const { connectionString } = require('pg/lib/defaults');
 
 const app = express();
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
-// Rota de Cadastro
-app.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const result = await pool.query(
-            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email',
-            [name, email, hashedPassword]
-        );
-
-        res.status(201).json(result.row[0]);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Erro ao registrar usuário"});
-    }
+app.get('/', (req, res) => {
+    res.send('Hello');
 });
 
-// Rota de login
-app.post('/login', async(req, res) => {
-    const{email, password} = req.body;
-
-    try{
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        if(result.rows.length === 0) return res.status(401).json({ error: "Usuário não encontrado." });
-
-        const user = result.rows[0];
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch) return res.status(401).json({ error: "Senha incorreta."});
-
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h'});
-
-        res.json({ token, user: { id: user.id, name: user.name, email: user.email } });
-    } catch(err){
-        console.error(err);
-        res.status(500).json({ error: "Erro ao logar."});
-    }
+app.listen(PORT, () => {
+    console.log(`Servidor a rodar na porta ${PORT}`);
 })
-
-// Server start
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,38 +1,38 @@
-document.getElementById("registerForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
+// document.getElementById("registerForm").addEventListener("submit", async function (e) {
+//     e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:5000/register", {
-        method: "POST",
-        headers: { "Content-type": "application/json"},
-        body: JSON.stringify({ name, email, password }),
-    });
+//     const response = await fetch("http://localhost:5000/register", {
+//         method: "POST",
+//         headers: { "Content-type": "application/json"},
+//         body: JSON.stringify({ name, email, password }),
+//     });
 
-    const data = await response.json();
-    document.getElementById("message").textContent = data.error || "Registro feito!"
-})
+//     const data = await response.json();
+//     document.getElementById("message").textContent = data.error || "Registro feito!"
+// })
 
-document.getElementById("loginForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
+// document.getElementById("loginForm").addEventListener("submit", async function (e) {
+//     e.preventDefault();
 
-    const email = document.getElementById("loginEmail").value;
-    const password = document.getElementById("loginPassword").value;
+//     const email = document.getElementById("loginEmail").value;
+//     const password = document.getElementById("loginPassword").value;
 
-    const response = await fetch("http://localhost:5000/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-    });
+//     const response = await fetch("http://localhost:5000/login", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ email, password }),
+//     });
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    if(data.token){
-        localStorage.setItem("token", data.token);
-        document.getElementById("message").textContent = "Logado com sucesso!";
-    } else {
-        document.getElementById("message").textContent = data.error;
-    }
-});
+//     if(data.token){
+//         localStorage.setItem("token", data.token);
+//         document.getElementById("message").textContent = "Logado com sucesso!";
+//     } else {
+//         document.getElementById("message").textContent = data.error;
+//     }
+// });
